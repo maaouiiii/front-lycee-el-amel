6cd03820b08362ad710d486322747d231bfb5f36
Karim maaoui <hama.maaoui.6@gmail.com>
2023-01-29 20:31:06 +0100

    add all interfaces

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,18 +4,28 @@ import { IndexComponent } from './pages/index/index.component';
 import { RouterModule, Routes } from '@angular/router';
 import { FooterComponent } from './pages/layout/footer/footer.component';
 import { HomeComponent } from './pages/layout/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { TeachersComponent } from './pages/teachers/teachers.component';
+import { PricingComponent } from './pages/pricing/pricing.component';
+import { EventsComponent } from './pages/events/events.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent,
     children: [
        {path: 'index', component: IndexComponent},
+       {path: 'about', component: AboutComponent},
+       {path: 'teachers', component: TeachersComponent},
+       {path: 'events', component: EventsComponent},
+       {path: 'pricing', component: PricingComponent},
+       {path: 'contact', component: ContactComponent},
+
        { path: '', redirectTo: 'index', pathMatch: 'full' },
 
 ]}, /*
     {path: '404', component: FooterComponent},
     { path: '**', redirectTo: '404' }*/
 ];
-
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,11 @@ import { FooterComponent } from './pages/layout/footer/footer.component';
 import { IndexComponent } from './pages/index/index.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './pages/layout/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { TeachersComponent } from './pages/teachers/teachers.component';
+import { EventsComponent } from './pages/events/events.component';
+import { PricingComponent } from './pages/pricing/pricing.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +19,12 @@ import { HomeComponent } from './pages/layout/home/home.component';
     NavbarComponent,
     FooterComponent,
     IndexComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent,
+    TeachersComponent,
+    EventsComponent,
+    PricingComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
