7925805fa40b94b69e3be66d8cba8266bcbc0c7d
Karim maaoui <hama.maaoui.6@gmail.com>
2023-02-06 20:16:32 +0100

    personalize

--- src/assets/vendor/main.js
+++ src/assets/vendor/main.js
@@ -62,7 +62,7 @@
     on('click', '.mobile-nav-toggle', function(e) {
       select('#navbar').classList.toggle('navbar-mobile')
       this.classList.toggle('bi-list')
-      this.classList.toggle('bi-x')
+      this.classList.toggle('bi bi-list mobile-nav-toggle')
     })
   
     /**
